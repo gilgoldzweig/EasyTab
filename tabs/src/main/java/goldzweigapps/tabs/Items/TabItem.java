a29d00df2f0b101d1b111e59c08433e136a57ddb
gilgoldzweig <gil5841@gmail.com>
2016-10-27 16:05:27 +0300

    bugdsg

---
+++ tabs/src/main/java/goldzweigapps/tabs/Items/TabItem.java
@@ -0,0 +1,32 @@
+package goldzweigapps.tabs.Items;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by gilgoldzweig on 13/05/16.
+ */
+public class TabItem {
+    private Fragment fragment;
+    private String title;
+
+    public TabItem(Fragment fragment, String title) {
+        this.fragment = fragment;
+        this.title = title;
+    }
+
+    public Fragment getFragment() {
+        return fragment;
+    }
+
+    public void setFragment(Fragment fragment) {
+        this.fragment = fragment;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
